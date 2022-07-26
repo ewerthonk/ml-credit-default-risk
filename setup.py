9e3e74aaedc96434b3d49c27ab6856d7e12d126e
ewerthonk <kutz.ewerthon@gmail.com>
2022-07-26 11:48:18 -0300

    Creating Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description="Can we use AI to predict if a borrower will pay his debt? Let's find out. My 2nd Machine Learning Project.",
+    author='Ewerthon Jose Kutz',
+    license='MIT',
+)
